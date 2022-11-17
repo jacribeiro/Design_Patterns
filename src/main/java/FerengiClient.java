05664d3b687d2647500b18eda61b351aed7e721c
Joao Ribeiro <j.alberto.cribeiro@gmail.com>
2022-11-17 11:42:17 +0000

    Created AlienClient abstract class and concrete classes that extend it, in order to observe the Factory Method pattern

---
+++ src/main/java/FerengiClient.java
@@ -0,0 +1,8 @@
+public class FerengiClient extends AlienClient {
+
+    @Override
+    protected OrderingStrategy createOrderingStrategy() {
+        OrderingStrategy strategy = new SmartStrategy();
+        return strategy;
+    }
+}
